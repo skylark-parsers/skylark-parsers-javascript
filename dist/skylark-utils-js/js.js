f8e5aaf1544bb61d673aad7ceb69a6b2f7de760f
hudaokeji <github@hudaokeji.com>
2018-09-09 20:51:28 +0900

    init commit

---
+++ dist/skylark-utils-js/js.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-utils-js - The js features enhancement for skylark utils.
+ * @author Hudaokeji Co.,Ltd
+ * @version v0.9.0
+ * @link www.skylarkjs.org
+ * @license MIT
+ */
+define(["skylark-utils/skylark","skylark-utils/scripter"],function(r,k){return k});
+//# sourceMappingURL=sourcemaps/js.js.map
---
+++ dist/uncompressed/skylark-utils-js/js.js
@@ -0,0 +1,7 @@
+define([
+    "skylark-utils/skylark",
+    "skylark-utils/scripter"
+], function(skylark, scripter) {
+	
+	return scripter;
+});
