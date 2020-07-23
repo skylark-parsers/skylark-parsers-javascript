c9aa065652fd8f349ac24ce2cfeac098dde8997a
hudaokeji <github@hudaokeji.com>
2020-07-23 21:26:21 +0900

    update

--- dist/skylark-scripts-javascript/Parser.js
+++ dist/skylark-scripts-javascript/Parser.js
@@ -1,5 +1,5 @@
 /**
- * skylark-parsers-javascript - The js features enhancement for skylark utils.
+ * skylark-scripts-javascript - The js features enhancement for skylark utils.
  * @author Hudaokeji Co.,Ltd
  * @version v0.9.0
  * @link www.skylarkjs.org
